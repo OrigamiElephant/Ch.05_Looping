0c83f57e0d346c8f0db4b78d12a0226396fc9a64
OrigamiElephant <mcculleyezr@urbandaleschools.com>
2019-02-01 11:58:34 -0600

    Doing chapter 5

--- 5.1_Coin_Toss.py
+++ 5.1_Coin_Toss.py
@@ -6,9 +6,19 @@ COIN TOSS PROGRAM
 3.) Add a loop so that the program does this 50 times.
 4.) Create a running total for the number of heads and the number of tails and print the total at the end.
 '''
-for i in range
-    coin = (1,0)
-    print(coin)
+h = 0
+t = 0
+import random
+for x in range(50):
+    c = (random.randint(0,1))
+    if c == 1:
+        print("Heads")
+        h +=1
+    else:
+        print("Tails")
+        t +=1
+print("# of Heads:",h)
+print("# of Tails:",t)
 
 
 
--- 5.2_Roshambo.py
+++ 5.2_Roshambo.py
@@ -10,6 +10,57 @@ Add conditional statements to figure out who wins and keep the records
 When the user quits print a win/loss record
 
 '''
+import random
+i = 1
+p = 0
+win = 0
+tie = 0
+lose = 0
+while i == 1:
+    n = input("Rock(1), Paper(2), Or Scissors(3)?")
+    if n == "1":
+        p = 1
+    elif n == "2":
+        p = 2
+    elif n == "3":
+        p = 3
+    else:
+        print("Please input the number next to your choice")
+    cpu = random.randint(1, 3)
+    if cpu == 1:
+        print("Rock")
+    elif cpu == 2:
+        print("paper")
+    else:
+        print("Scissors")
+    if cpu == p:
+        print("Tie")
+        tie += 1
+    elif cpu == 1 and p == 2:
+        print("Paper beats Rock. You Win")
+        win += 1
+    elif cpu == 1 and p == 3:
+        print("Rock beats Scissors. You Lose")
+        lose += 1
+    elif cpu == 2 and p == 1:
+        print("Paper beats Rock. You Lose")
+        lose += 1
+    elif cpu == 2 and p == 3:
+        print("Scissors beat Paper. You win")
+        win += 1
+    elif cpu == 3 and p == 1:
+        print("Rock beats Scissors. You Win")
+        win += 1
+    elif cpu == 3 and p == 2:
+        print("Scissors beat Paper. You Lose")
+        lose += 1
+    i = input("Do you wish to play again? Yes(1) or No(2)")
+    if input == 2:
+        i = 0
+print("Ties", tie)
+print("wins", win)
+print("Loses", lose)
+
 
 
 
